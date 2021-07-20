64ca4bd1607bc393d909f24308e1d2890eb92d8e
Thiel Serra <salathiel.serra@stc.ma.gov.br>
2021-07-20 18:56:46 -0300

    Adicionando model Category e mapeando os relacionamentos necessários com produtos

--- app/Product.php
+++ app/Product.php
@@ -10,4 +10,9 @@ class Product extends Model
     {
         return $this->belongsTo(Store::class);
     }
+
+    public function Categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
