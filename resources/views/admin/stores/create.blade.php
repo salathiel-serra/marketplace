2836f208770c4c85e03f8d5d91f77325263d4331
Thiel Serra <salathiel.serra@stc.ma.gov.br>
2021-07-22 18:42:30 -0300

    Melhorando layout do projeto com adição do CDN Bootstrap e criação de arquivo de específico para base do layout extendendo as demais páginas

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,41 +1,46 @@
-<h1>Criar loja</h1>
-<form action="{{url('admin/stores/store')}}" method="post">
-  <input type="hidden" name="_token" value="{{csrf_token()}}">
-  <div>
-    <label for="">Nome loja</label>
-    <input type="text" name="name">
-  </div>
+@extends('layouts.app')
 
-  <div>
-    <label for="">Descrição</label>
-    <input type="text" name="description">
-  </div>
+@section('content')
+  <h1>Criar loja</h1>
+  <form action="{{url('admin/stores/store')}}" method="post">
+    <input type="hidden" name="_token" value="{{csrf_token()}}">
+    <div class="form-group">
+      <label for="">Nome loja</label>
+      <input type="text" name="name" class="form-control">
+    </div>
 
-  <div>
-    <label for="">Telefone</label>
-    <input type="text" name="phone">
-  </div>
+    <div class="form-group">
+      <label for="">Descrição</label>
+      <input type="text" name="description" class="form-control">
+    </div>
 
-  <div>
-    <label for="">Celular/WhatsApp</label>
-    <input type="text" name="mobile_phone">
-  </div>
+    <div class="form-group">
+      <label for="">Telefone</label>
+      <input type="text" name="phone" class="form-control">
+    </div>
 
-  <div>
-    <label for="">Slug</label>
-    <input type="text" name="slug">
-  </div>
+    <div class="form-group">
+      <label for="">Celular/WhatsApp</label>
+      <input type="text" name="mobile_phone" class="form-control">
+    </div>
 
-  <div>
-    <label for="">Usuário</label>
-    <select name="user" id="">
-      @foreach($users as $user)
-        <option value="{{$user->id}}"> {{$user->name}} </option>
-      @endforeach
-    </select>
-  </div>
+    <div class="form-group">
+      <label for="">Slug</label>
+      <input type="text" name="slug" class="form-control">
+    </div>
 
-  <div>
-    <button type="submit">Enviar</button>
-  </div>
-</form>
+    <div class="form-group">
+      <label for="">Usuário</label>
+      <select name="user" id="" class="form-control">
+        @foreach($users as $user)
+          <option value="{{$user->id}}"> {{$user->name}} </option>
+        @endforeach
+      </select>
+    </div>
+
+    <div class="form-group">
+      <button type="submit" class="btn btn-success btn-large">Enviar</button>
+    </div>
+  </form>
+@endsection
+
