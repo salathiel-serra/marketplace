744cc5ed8dd4dd6e581ad140878bf1d1c46fd4fd
Thiel Serra <salathiel.serra@stc.ma.gov.br>
2021-07-26 20:19:31 -0300

    Adicionando siderbar ao layout da aplicação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,38 @@
 
 </head>
 <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom:40px;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="{{route('home')}}">Marketplace</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        @auth
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link @if(request()->is('admin/stores')) active @endif" aria-current="page" href="{{route('admin.stores.index')}}">Loja</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link @if(request()->is('admin/products')) active @endif"" href="{{route('admin.products.index')}}">Produtos</a>
+            </li>
+          </ul>
+          <div class="my-2 my-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#" onclick="event.preventDefault(); 
+                                                      document.querySelector('form.logout').submit(); "> Sair </a>
+
+                  <form action="{{route('logout')}}" class="logout" method="post" style="display:none;">
+                    @csrf
+                  </form>
+              </li>
+            </ul>
+          </div>
+        @endauth
+      </div>
+    </div>
+  </nav>
   <div class="container">
     @include('flash::message')
     @yield('content')
